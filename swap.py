3b3e8ee7f923c5fe77d3154642ac0799fe43e235
LATHA022 <109948643+LATHA022@users.noreply.github.com>
2022-07-25 13:32:02 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,10 @@
+x = input()
+y = input()
+print('The value of x is {}'.format(x))
+print('The value of y is {}'.format(y))
+# swapping the values
+z = x
+x = y
+y = z
+print('The value of x after swapping is {}'.format(x))
+print('The value of y after swapping is {}'.format(y))
